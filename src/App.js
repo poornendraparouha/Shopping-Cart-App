702b23056d5ef7c6aef456da5ada1422025c1c87
Poornendra Parouha <poornendraparouha@gmail.com>
2024-08-10 12:34:06 +0530

    costom provider craeted

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import './App.css';
-import { useState } from 'react';
 import Items from './components/Items';
 import Navbar from './components/Navbar';
-import { itemContext } from './itemContext';
+import CustomItemContext from './itemContext';
 
 
 function App() {
-  const [total, setTotal] = useState(0);
-  const [item, setItem] = useState(0);
+
   return (
-    <itemContext.Provider value={{item, setItem, total, setTotal}}>
-        <div className='App'>
+    // <itemContext.Provider value={{item, setItem, total, setTotal}}>
+    <CustomItemContext>
+          <div className='App'>
           <h2>Shopping Cart</h2>
             <Navbar />
             <Items />
         </div>
-      </itemContext.Provider>
+
+    </CustomItemContext>
   );
 }
 export default App;
--- src/itemContext.js
+++ src/itemContext.js
@@ -1,5 +1,20 @@
 import { createContext } from "react";
+import { useState } from 'react';
+
 
 const itemContext = createContext();
 
+function CostomItemContext({children}){
+    const [total, setTotal] = useState(0);
+    const [item, setItem] = useState(0);
+    // console.log(props)
+    return(
+        <itemContext.Provider value={{item, setItem, total, setTotal}}>
+            {children}
+
+        </itemContext.Provider>
+    )
+}
+
 export {itemContext};
+export default CostomItemContext;
